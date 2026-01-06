8e8e21b7064c1482f75e0fc367b5298414cf779c
aminsarshar <sarsharamin85@gmail.com>
2026-01-06 12:06:19 +0330

    Refactor Blade views admin and split layout into sections

---
+++ resources/views/home/sections/header.blade.php
@@ -0,0 +1,33 @@
+<header class="header">
+      <div class="container">
+        <nav class="nav" data-aos="zoom-in" data-aos-duration="800">
+          <h1 class="nav__logo">AS Dev</h1>
+          <ul class="menu">
+            <li data-section="home" class="menu__item">
+              <a href="#" class="menu__link menu__link--active">خانه</a>
+            </li>
+            <li data-section="about-us" class="menu__item">
+              <a href="#" class="menu__link">درباره من </a>
+            </li>
+            <li data-section="resume" class="menu__item">
+              <a href="#" class="menu__link">رزومه من</a>
+            </li>
+            <li data-section="services" class="menu__item">
+              <a href="#" class="menu__link">خدمات </a>
+            </li>
+            <li data-section="portfolio" class="menu__item">
+              <a href="#" class="menu__link">نمونه کار ها</a>
+            </li>
+            <li data-section="pricing" class="menu__item">
+              <a href="#" class="menu__link">قیمت ها</a>
+            </li>
+            <li data-section="contactus" class="menu__item">
+              <a href="#" class="menu__link">ارتباط با من</a>
+            </li>
+          </ul>
+          <div class="nav__toggle-icon">
+            <div class="nav__toggle-line"></div>
+          </div>
+        </nav>
+      </div>
+    </header>
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('admin.layouts.admin');
+    return view('home.index');
+});
+
+
+Route::get('/admin-panel/dashboard', function () {
+    return view('admin.dashboard');
 });
